2cd57d7112f3e644cb10336031828bbce3ac2961
hrecker <henryjrecker@gmail.com>
2023-02-20 11:30:41 -0600

    Add weather countdown and tints

--- webroot/scripts/scenes/UIScene.ts
+++ webroot/scripts/scenes/UIScene.ts
@@ -13,6 +13,8 @@ export class UIScene extends Phaser.Scene {
     scoreText: Phaser.GameObjects.BitmapText;
 
     weatherImages: Phaser.GameObjects.Image[];
+    weatherImageBorders: Phaser.GameObjects.Image[];
+    weatherCountdownText: Phaser.GameObjects.BitmapText;
 
     constructor() {
         super({
@@ -32,7 +34,9 @@ export class UIScene extends Phaser.Scene {
         for (let i = 0; i < this.weatherImages.length; i++) {
             let pos = this.weatherImages.length - i - 1;
             this.weatherImages[i].setPosition(rightX - (pos * weatherImageWidth), uiY);
+            this.weatherImageBorders[i].setPosition(this.weatherImages[i].x, this.weatherImages[i].y);
         }
+        this.weatherCountdownText.setPosition(this.weatherImages[0].x, this.weatherImages[0].y - 5);
     }
 
     init(data) {
@@ -47,9 +51,13 @@ export class UIScene extends Phaser.Scene {
         this.scoreText = this.add.bitmapText(0, 0, "uiFont", "0", 48).setOrigin(0, 0.5);
         // Weather queue images
         this.weatherImages = [];
+        this.weatherImageBorders = [];
+        let tints = config()["weatherPreviewTints"];
         for (let i = 0; i < this.gardenGame.weatherQueue.length; i++) {
-            this.weatherImages.push(this.add.image(0, 0, this.gardenGame.weatherQueue[i] + "Preview"));
+            this.weatherImages.push(this.add.image(0, 0, this.gardenGame.weatherQueue[i] + "Preview").setTint(parseInt(tints[i], 16)));
+            this.weatherImageBorders.push(this.add.image(0, 0, "toolbox"));
         }
+        this.weatherCountdownText = this.add.bitmapText(0, 0, "uiFont", "", 36).setOrigin(0.5);
         this.resize(true);
         this.scale.on("resize", this.resize, this);
     }
@@ -70,5 +78,11 @@ export class UIScene extends Phaser.Scene {
         for (let i = 0; i < weatherQueue.length; i++) {
             scene.weatherImages[i].setTexture(weatherQueue[i] + "Preview");
         }
+        scene.weatherCountdownText.setText(Math.ceil(config()["weatherDurationMs"] / 1000).toString());
+    }
+
+    update() {
+        let weatherTimeRemaining = config()["weatherDurationMs"] - this.gardenGame.currentWeatherDurationMs;
+        this.weatherCountdownText.setText(Math.ceil(weatherTimeRemaining / 1000).toString());
     }
 }
