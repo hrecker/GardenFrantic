1edd9887d7bd737a749ca2e91e9fb1375e811db0
hrecker <henryjrecker@gmail.com>
2023-05-07 15:07:22 -0500

    Add loading progress bar

--- webroot/scripts/scenes/LoadingScene.ts
+++ webroot/scripts/scenes/LoadingScene.ts
@@ -4,10 +4,14 @@ import { Tool } from "../game/Tool";
 import { Weather } from "../game/Weather";
 import { loadConfig } from "../model/Config";
 
-let loadingText: Phaser.GameObjects.Text;
 
 /** Load json and assets */
 export class LoadingScene extends Phaser.Scene {
+    loadingText: Phaser.GameObjects.Text;
+    loadingBox: Phaser.GameObjects.Rectangle;
+    loadingFill: Phaser.GameObjects.Rectangle;
+    maxQueueSize: number;
+
     constructor() {
         super({
             key: "LoadingScene"
@@ -19,19 +23,15 @@ export class LoadingScene extends Phaser.Scene {
         if (! this.scene.isActive() && ! force) {
             return;
         }
-        loadingText.setPosition(this.game.renderer.width / 2, this.game.renderer.height / 2);
+        this.loadingText.setPosition(this.game.renderer.width / 2, this.game.renderer.height / 2 - 50);
+        
+        this.loadingFill.setPosition(this.loadingText.x, this.loadingText.y + 100);
+        this.loadingFill.setSize(this.loadingText.width, this.loadingText.height / 2);
+        this.loadingBox.setPosition(this.loadingText.x, this.loadingText.y + 100);
+        this.loadingBox.setSize(this.loadingText.width, this.loadingText.height / 2);
     }
 
-    preload() {
-        // Loading message
-        // Have to hard-code this because the config isn't loaded yet
-        this.cameras.main.setBackgroundColor("#ffffff");
-        loadingText = this.add.text(0, 0, "Loading...",
-            { font: "bold 64px Verdana",
-            stroke: "black",
-            strokeThickness: 3,
-            color: "#FFF7E4" }).setOrigin(0.5, 0.5);
-
+    loadResources() {
         // Ensure the canvas is the right size
         this.scale.refresh();
         this.resize(true);
@@ -151,6 +151,14 @@ export class LoadingScene extends Phaser.Scene {
 
         // Load json
         this.load.json("config", "assets/json/config.json");
+        this.load.start();
+        this.load.on('complete', () => {
+            // Start the main menu scene
+            loadConfig(this.cache.json.get("config"));
+            this.scene.start("BackgroundScene")
+                      .start("MenuScene")
+                      .stop();
+        })
     }
 
     loadToolTexture(tool: Tool) {
@@ -178,9 +186,20 @@ export class LoadingScene extends Phaser.Scene {
     }
 
     create() {
-        loadConfig(this.cache.json.get("config"));
-        this.scene.start("BackgroundScene")
-                  .start("MenuScene")
-                  .stop();
+        // Loading message
+        // Have to hard-code this because the config isn't loaded yet
+        this.cameras.main.setBackgroundColor("#8CABA1");
+        this.loadingText = this.add.text(0, 0, "Loading...",
+            { font: "bold 64px Verdana",
+            stroke: "black",
+            strokeThickness: 3,
+            color: "#FFF7E4" }).setOrigin(0.5, 0.5);
+        this.loadingFill = this.add.rectangle(0, 0, 0, 0, 0xFFF7E4, 1);
+        this.loadingBox = this.add.rectangle(0, 0, 0, 0).setStrokeStyle(3, 0x000000, 1);
+        this.loadResources();
+    }
+
+    update() {
+        this.loadingFill.width = this.loadingBox.width * this.load.progress;
     }
 }
